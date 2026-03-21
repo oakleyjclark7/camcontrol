3a1dfb2a1aa2c2cacc6bd1c274b3531128bf1309
Oakley <oakley.clark@adaptix.com>
2026-03-21 23:00:01 +0000

    setup streaming example program

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 ﻿#include <camera/camera_image.hpp>
+#include <camera/streaming.hpp>
+
 #include <print>
+
+using namespace camcontrol;
 
 int main()
 {
-
-    std::println("Hello camera control");
-
+    std::println("Hello cameracontrol");
     return 0;
 }
